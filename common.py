12b14ca87e02138e04e90f977e8db92fb6e91d55
Henry Zhang <zhangxuhang15@hotmail.com>
2025-01-16 15:02:06 +0800

    Common Modification

--- common.py
+++ common.py
@@ -12,3 +12,17 @@ def is_prime_number(n):
         if n % i == 0:
             return False
     return True
+
+
+def get_factors(n):
+    factors_front = []
+    factors_behind = []
+
+    for i in range(1, int(sqrt(n)) + 1):
+        if n % i == 0:
+            factors_front.append(i)
+            factors_behind.append(n // i)
+    if factors_front[-1] == factors_behind[-1]:
+        factors_behind.pop()
+    factors_behind.reverse()
+    return factors_front + factors_behind
